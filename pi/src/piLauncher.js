f10a8908d76404f74ef8f59c8458ee3a522e1d34
Steveo <steveolyo@gmail.com>
2018-12-29 13:37:21 -0700

    Led Blink code

--- pi/src/piLauncher.js
+++ pi/src/piLauncher.js
@@ -1,1 +1,13 @@
-//code here
+
+const five = require('johnny-five');
+const Raspi = require('raspi-io');
+
+const board = Five.board({
+    io: new Raspi()
+});
+
+board.on('ready', () => {
+    const led = new five.Led('P1-12');
+    led.blink();
+});
+
