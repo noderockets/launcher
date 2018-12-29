0cdef0d2aafd34e0ab4fd04604c0f1a4f87dfe62
Steveo <steveolyo@gmail.com>
2018-12-28 20:03:49 -0700

    Adding pi-zero launcher code

---
+++ app/pi-zero-launcher/src/launcher.js
@@ -0,0 +1,12 @@
+const five = require('johhny-five');
+const Raspi = require('raspi-io');
+
+const board = Five.board({
+    io: new Raspi()
+});
+
+board.on('ready', () => {
+    const led = new five.
+});
+
+//
